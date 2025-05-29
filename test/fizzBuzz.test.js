08483e0a5cde83e4e6280ba5c89ef31f6dedf9e0
JlBestMc <jlrcmt@gmail.com>
2025-05-29 13:02:50 +0200

    wip fizzbuzz tests

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -1,5 +1,5 @@
 import { expect, describe, test } from "@jest/globals";
-import { fizzBuzz } from "../src/core/fizzBuzz.js";
+import fizzBuzz from "../src/core/fizzBuzz.js";
 
 describe('FizzBuzz test for multiples of 3 and 5', () => {
        
@@ -44,7 +44,7 @@ describe('FizzBuzz test for multiples of 3 and 5', () => {
             let valor_entrada = 10;
             let respuesta_esperada = {
                 status: "ok", // Código indicando éxito
-                message: "El número es divisible por 3", // Mensaje de validación
+                message: "El número es divisible por 5", // Mensaje de validación
                 data: {
                     input: 10,  // Número evaluado
                     output: "Buzz" // Resultado esperado
@@ -68,7 +68,27 @@ describe('FizzBuzz test for multiples of 3 and 5', () => {
              * When el número es procesado
              * Then se muestra "FizzBuzz"
              */
-        })
+
+             // Arrange
+            let valor_entrada = 15;
+            let respuesta_esperada = {
+                status: "ok", // Código indicando éxito
+                message: "El número es divisible por 3 y 5", // Mensaje de validación
+                data: {
+                    input: 15,  // Número evaluado
+                    output: "FizzBuzz" // Resultado esperado
+                }}
+
+            // Act
+            let resultado = fizzBuzz(valor_entrada);
+    
+            // Assert
+            expect(typeof resultado.data.input).toBe("number"); // valida que input sea un número
+            expect(resultado).toEqual(respuesta_esperada); // valida estructura de respuesta
+            expect(resultado.data.output).toBe("FizzBuzz"); // valida que el valor output corresponda al expect
+
+
+        });
 
         test('returns number when is not multiple of 3 and 5', () => {
             //Gherking test
@@ -78,6 +98,12 @@ describe('FizzBuzz test for multiples of 3 and 5', () => {
              * When el número es procesado
              * Then se muestra el número ingresado
              */
-        })
+
+
+            
+
+
+            
+        });
     }
 )
