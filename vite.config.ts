8bceba7797d9f24c9d68a3e662a3e0283b8f5859
JlBestMc <jlrcmt@gmail.com>
2025-06-12 12:24:43 +0200

    Updates

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-const repoName = 'js-test-level-fizz-buzz'
-
-export default defineConfig({
-  base: `/${repoName}/`, // 👈 muy importante para GitHub Pages
-})
