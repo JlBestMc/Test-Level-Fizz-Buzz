ba80e3523a212e5532ae059fc2d5d61f12b3d425
JlBestMc <jlrcmt@gmail.com>
2025-06-16 04:09:20 +0200

    events.js added

---
+++ src/ui/events.js
@@ -0,0 +1,22 @@
+
+import { fizzBuzz } from '../core/fizzBuzz.js';
+import { addToHistory } from './dom.js';
+
+const form = document.getElementById('fizzbuzz-form');
+const inputNumber = document.getElementById('number-input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const valueText = inputNumber.value.trim();
+
+  if (valueText=== "") {
+    alert("Por favor, ingresa un número válido.");
+    return;
+  }
+
+  const value = Number(valueText);
+  const result = fizzBuzz(value);
+
+  addToHistory(result);
+});
